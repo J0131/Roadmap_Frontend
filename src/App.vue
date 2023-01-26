4e6c1d83639d85ddd3ed7b64d03398015fb445fc
JJune <iwhy7979@naver.com>
2023-01-26 17:36:00 +0900

    [ADD] SideBar 추가 구현

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
   <div id="app">
     <main-map/>
+    <side-bar class="side-bar"/>
   </div>
 </template>
 
 <script>
 import MainMap from './components/MainMap.vue'
+import SideBar from './components/SideBar.vue'
 
 export default {
   name: 'App',
   components: {
-    MainMap
+    MainMap,
+    SideBar
   }
 }
 </script>
 
-<style>
+<style lang = "scss">
 #app {
   position: absolute;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
-}
+
+  > .side-bar {
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+  }
+} 
+
 </style>
